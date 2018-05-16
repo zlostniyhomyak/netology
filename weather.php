deadc8d6ebc24916633c5cb569c277a3ed461dbf
Roman <shesternin@sph.kz>
2018-05-16 19:21:43 +0600

    Initial commit.

---
+++ weather.php
@@ -0,0 +1,16 @@
+<?php 
+$weather = file_get_contents('http://api.openweathermap.org/data/2.5/weather?q=Almaty,kz&APPID=2b86350d3431c066d2b104c4b64757e2');
+$json = json_decode($weather);
+// print_r($json);
+$json2 = json_encode($json, JSON_PRETTY_PRINT);
+// var_dump($json2);
+if ($weather != null) {
+	$file = fopen("C:\\wamp64\\www\\Netology\\log.json", "w");
+	fwrite($file, $json2);
+	fclose($file);
+};
+$file2 = fopen("C:\\wamp64\\www\\Netology\\log.json", "r");
+$file3 = readfile("C:\\wamp64\\www\\Netology\\log.json");
+// $array = file($file2);
+
+?>
